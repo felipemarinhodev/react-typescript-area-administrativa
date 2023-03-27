fc2842b3545014fd59cce78827ecd060d39f7e32
felipemarinhodev <felipmarinho@gmail.com>
2023-03-27 12:31:17 -0700

    a3.3 - challenge static data

--- src/components/Tabela/index.tsx
+++ src/components/Tabela/index.tsx
@@ -8,12 +8,21 @@ export function Tabela() {
           <TableHead>
             <TableRow>
               <TableCell >Data</TableCell>
+              <TableCell >Horário</TableCell>
+              <TableCell >Profissional</TableCell>
+              <TableCell >Especialidade</TableCell>
+              <TableCell >Paciente</TableCell>
+              <TableCell >Modalidade</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             <TableRow>
               <TableCell component="th" scope="row">22/03/2022</TableCell>
               <TableCell>08:30</TableCell>
+              <TableCell>Desenvolvedor</TableCell>
+              <TableCell>React JS</TableCell>
+              <TableCell>Metronet</TableCell>
+              <TableCell>Contractor</TableCell>
             </TableRow>
           </TableBody>
         </Table>
