0924bcd37fa07f84f211e94fe5fde7732f03f7a1
felipemarinhodev <felipmarinho@gmail.com>
2023-03-27 10:39:59 -0700

    a1.4 Container

---
+++ src/components/Container/index.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.section`
+  background-color: var(--cinza-claro);
+  margin: 4em;
+  border-radius: 4em;
+  padding: 2em;
+  height: 100%;
+`
+
