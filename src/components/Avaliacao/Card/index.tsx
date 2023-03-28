0836d5e8e7b82bd82815983a3d2ebea7317b0198
felipemarinhodev <felipmarinho@gmail.com>
2023-03-27 17:52:18 -0700

    a5.4 - challenge - practice CSS

--- src/components/Avaliacao/Card/index.tsx
+++ src/components/Avaliacao/Card/index.tsx
@@ -15,32 +15,66 @@ const ContainerEstelizado = styled.div`
   border-radius: 8px;
   color: var(--cinza);
   margin: 1em 2em 1em;
+  `
+
+const ImagemEstilizada = styled.img`
+    border-radius: 100%;
+    width: 64px;
+    object-fit: cover;
+    height: 64px;
+    border: 2px solid var(--azul-claro);
+    margin-right: 1em;
 `
 
+const ListaEstilizada = styled.ul`
+    display: flex;
+    align-items: center;
+    width: 100%;
+    justify-content: space-between;
+    padding: 0;
+`
+
+const ItemEstilizado = styled.li`
+  list-style-type: none;
+`
+
+const ItemInformacoesEstilizado = styled(ItemEstilizado)`
+    flex-grow: 1;
+`
+
+const ParagrafoEstilizado = styled.p`
+    margin: .5em 0;
+    font-size: 14px;
+`
+
+const ParagrafoNomeEstilizado = styled.p`
+    font-weight: 700;
+`
 
 export function Card({ profissional }: CardProps) {
   return (
 
     <ContainerEstelizado>
-      <ul>
-        <li>
-          <img
+      <ListaEstilizada>
+        <ItemEstilizado>
+          <ImagemEstilizada
             src={profissional.imagem}
             alt={`Foto de perfil do profissional ${profissional.nome}`}
           />
-        </li>
-        <li>
-          <p>{profissional.nome}</p>
-          <p>{profissional.especialidade}</p>
-        </li>
-        <li>
+        </ItemEstilizado>
+        <ItemInformacoesEstilizado>
+          <ParagrafoNomeEstilizado>{profissional.nome}</ParagrafoNomeEstilizado>
+          <ParagrafoEstilizado>{profissional.especialidade}</ParagrafoEstilizado>
+        </ItemInformacoesEstilizado>
+        <ItemEstilizado>
           <Rating
             name="simple-controlled"
             value={profissional.nota}
             readOnly
+            size="large"
           />
-        </li>
-      </ul>
+        </ItemEstilizado>
+      </ListaEstilizada>
     </ContainerEstelizado>
   );
 }
